92058915ac3849d5ee0e6cb343b5f7d07756d5b3
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2026-03-17 21:36:39 +0530

    handled Main Class

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,17 +1,27 @@
 package bank;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args)
+    {
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new SavingAccount("SA100", 5000));
+        accounts.add(new CurrentAccount("CA200", 3000));
+
+        for (Account acc : accounts) {
+            acc.deposit(1000);  // deposit money
+
+            try {
+                acc.withdraw(2000);
+                acc.withdraw(5000);
+            } catch (InsufficientFundsException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println(acc.accNumber + " balance: " + acc.getBalance());
+            System.out.println("----------------------");
+    }
     }
 }
